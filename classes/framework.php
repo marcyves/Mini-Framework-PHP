a3b484ad6c91240f140da9ad2e6cbd4b62d16f99
Marc Augier <m.augier@me.com>
2020-06-10 16:13:41 +0200

    ajout d'un menu dynamique

--- classes/framework.php
+++ classes/framework.php
@@ -23,21 +23,52 @@ function setDate($date)
     $this->prepare_page("date", $date);
 }
 
+function setMenu($page)
+{
+
+    $dossier_contenu = "contenu";
+
+    $menu = "<ul class='links'>";
+    if($d = opendir($dossier_contenu)){
+        while($fichier = readdir($d))
+        {
+            if ($fichier[0] != "."){
+        //            $fichier =str_replace(".html","",$fichier);
+                $pos = strpos($fichier,".");
+                $fichier = substr($fichier,0, $pos);
+                if($fichier == $page){
+                    $menu .= '<li class="active">';
+                }else{
+                    $menu .= '<li>';
+                }
+                $menu .= '<a href="exemple-framework.php?page='.$fichier.'">'.ucfirst(strtolower($fichier)).'</a></li>';
+            }
+        }
+        closedir($d);
+    }
+    $menu .= "</ul>";
+
+    $this->prepare_page("liens", $menu);
+}
+
 function setContenu()
 {
 
     if(isset($_GET['page'])){
         $page = $_GET['page'];
     }else{
-        $page = "home";
+        $page = "hoMe";
     }
 
     $fichier = "contenu/".$page.".html";
 
+    $this->setMenu($page);
+
     if (file_exists($fichier)){
         $contenu = file_get_contents($fichier);
     }else{
-        $contenu = "<h1>Erreur 404</h1>";
+        $contenu = "<h1>Erreur 404</h1>
+        <p>La page $page n'existe pas, si vous pensez que c'est une erreur, merci de contacter l'administrateur.</p>";
     }
 
     $this->prepare_page("contenu", $contenu);
--- exemple-framework.php
+++ exemple-framework.php
@@ -5,6 +5,7 @@ require_once("classes/framework.php");
 $ma_page = new Page("base");
 
 $ma_page->setTitre("Vive le GRETA");
+
 $ma_page->setDate("10 juin 2020");
 
 $ma_page->setContenu();
