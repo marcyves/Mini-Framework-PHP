27cdb5e3ebb2a27c33e6bf92371a899949e15280
Marc Augier <m.augier@me.com>
2020-07-24 17:38:55 +0200

    retour aux noms "standard"

--- site-dynamique/index.php
+++ site-dynamique/index.php
@@ -9,8 +9,8 @@ include_once "classes/Page.php";
 
 $ma_page = new Page();
 
-$ma_page->setDossierControleurs("controleur2/");
-$ma_page->setTheme("html5up-massively2");
+// $ma_page->setDossierControleurs("controleur2/");
+//$ma_page->setTheme("html5up-massively2");
 // $ma_page->setTemplate("catalogue");
 
 $ma_page->prepare();
