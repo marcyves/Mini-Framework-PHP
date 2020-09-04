20fd0538363b2633f1e1e32ffd23b7c5458903b4
Marc Augier <m.augier@me.com>
2020-09-04 19:21:48 +0200

    controleur catalogue simple

--- site-dynamique/controleurs/catalogue.php
+++ site-dynamique/controleurs/catalogue.php
@@ -2,12 +2,38 @@
 
 function controleur()
 {
+    $dossier_produits = "images/produits";
+
+      if(is_dir($dossier_produits)){
+
+        $tableau = "<tr>";
+        $compte  =  0;
+        if($d = opendir($dossier_produits))
+        {
+            while($image=readdir($d))
+            {
+                if ($image[0] != ".")
+                {
+                    $tableau .='<td><img width="300" src="'.$dossier_produits."/".$image.'"></td>';
+                    $compte++;
+                    if ($compte >= 3){
+                        $compte = 0;
+                        $tableau .= "</tr><tr>";
+                    }
+                }
+            }
+        }
+
+        $tableau = "<table>$tableau</table>";
+    }else{
+        $tableau = "<table><tr><td>pas de produits</tr></table>";
+    }
     return [
         'template' => 'catalogue',
         'titre' => "Le catalogue de nos produits",
         'sous-titre' => "Tout est fait main",
         'titre-court' => "Catalogue",
-        'tableau' => "<table><tr><td>Le tableau</td><td> en 2 colonnes</td></tr></table>",
+        'tableau' => $tableau,
         'contenu' => "Lorem ipsum toujours même pour le catalogue ...",
         'pied-de-page' => "Voici la fin de la page"
     ];
