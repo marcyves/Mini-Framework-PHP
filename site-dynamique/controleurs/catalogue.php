1a0f20e731718b21edcd42241b9d120c789a2a1b
Marc Augier <m.augier@me.com>
2021-05-02 20:05:04 +0200

    le controleur d'affichage du catalogue finalisé

--- site-dynamique/controleurs/catalogue.php
+++ site-dynamique/controleurs/catalogue.php
@@ -1,127 +1,135 @@
 <?php
 
-function creationVignette($fichier,$largeur,$hauteur){
+function creationVignette($dossier, $fichier, $extension, $largeur, $hauteur){
 
-    global $dossier_produits;
+    $fichier_source = $dossier."/".$fichier;
+    $fichier_sortie = $dossier."/mini_".$fichier;
 
-    $image_source = $dossier_produits."/".$fichier;
-    $image_sortie = $dossier_produits."/mini_".$fichier;
-
-	$system=explode('.',$image_source);
-    
-    switch ($system[1])
+    // Lecture du fichier dans un objet image PHP en fonction du type
+    switch($extension)
     {
-        case 'jpeg':
-        case 'jpg':
-            $src_img = imagecreatefromjpeg($image_source);
-            break;
-        case 'gif':
-            $src_img = imagecreatefromgif($image_source);
-            break;
-        case 'png':
-            $src_img = imagecreatefrompng($image_source);
-            break;
-        default:
-            return false;
-    }
-	
-	$largeur_avant=imageSX($src_img);
-    $hauteur_avant=imageSY($src_img);
-
-    $ratio = $largeur_avant/$hauteur_avant;
-    
-	if ($largeur_avant > $hauteur_avant) {
-		$thumb_w=$largeur;
-		$thumb_h=$largeur/$ratio;
-	}
-	if ($largeur_avant < $hauteur_avant) {
-		$thumb_w=$hauteur*$ratio;
-		$thumb_h=$hauteur;
-	}
-	if ($largeur_avant == $hauteur_avant) {
-		$thumb_w=$largeur;
-		$thumb_h=$hauteur;
-    }
-    
-    $dst_img=ImageCreateTrueColor($thumb_w,$thumb_h);
-	imagecopyresampled($dst_img,$src_img,0,0,0,0,$thumb_w,$thumb_h,$largeur_avant,$hauteur_avant); 
-    
-    switch ($system[1])
-    {
-        case 'jpeg':
-        case 'jpg':
-            imagejpeg($dst_img,$image_sortie); 
+        case "jpg":
+        case "jpeg":
+            $image_source = imagecreatefromjpeg($fichier_source);
         break;
-        case 'gif':
-            imagegif($dst_img,$image_sortie); 
+        case "png":
+            $image_source = imagecreatefrompng($fichier_source);
         break;
-        case 'png':
-            imagepng($dst_img,$image_sortie); 
+        case "gif":
+            $image_source = imagecreatefromgif($fichier_source);
         break;
         default:
             return false;
     }
-   
-	imagedestroy($dst_img); 
-    imagedestroy($src_img);
+
+    // Retaille l'image
+
+    $largeur_init = imageSX($image_source);
+    if($largeur_init < $largeur)
+    {
+        return false;
+    }
+    $hauteur_init = imageSY($image_source);
+    if($hauteur_init < $hauteur)
+    {
+        return false;
+    }
+
+    $ratio = $largeur/$hauteur;
+    $ratio_init = $largeur_init/$hauteur_init;
+
+    $image_destination = ImageCreateTrueColor($largeur, $hauteur);
     
-    return true;	
-}	
+    if($ratio == $ratio_init)
+    {
+        $largeur_dest = $largeur;
+        $hauteur_dest = $hauteur;
+        $dest_x = 0;
+        $dest_y = 0;
+    }else if ($ratio > $ratio_init){
+        $largeur_dest = $hauteur*$ratio_init;
+        $hauteur_dest = $hauteur;
+        $dest_x = $largeur/2 - $largeur_dest/2;
+        $dest_y = 0;
+    }else if ($ratio < $ratio_init){
+        $largeur_dest = $largeur;
+        $hauteur_dest = $largeur/$ratio_init;
+        $dest_x = 0;
+        $dest_y = $hauteur/2 - $hauteur_dest/2;
+    }
+
+    imagecopyresampled($image_destination, $image_source, $dest_x, $dest_y, 0, 0, $largeur_dest, $hauteur_dest, $largeur_init, $hauteur_init);
+    // Ecriture du fichier à partir de l'objet image PHP et en fonction du type
+    switch($extension)
+    {
+        case "jpg":
+        case "jpeg":
+            imagejpeg($image_destination, $fichier_sortie);
+        break;
+        case "png":
+            imagepng($image_destination, $fichier_sortie);
+        break;
+        case "gif":
+            imagegif($image_destination, $fichier_sortie);
+        break;
+        default:
+            return false;
+    }
+
+    return true;
+}
+
 
 function controleur()
 {
-    global $dossier_produits;
+    $extension_valide = array("jpg", "jpeg", "png", "gif");
+
+    $tableau = "<ul class='features'>";
 
     $dossier_produits = "images/produits";
 
-      if(is_dir($dossier_produits)){
-
-        $tableau = "";
-        $compte  =  0;
+    if(is_dir($dossier_produits))
+    {
         if($d = opendir($dossier_produits))
         {
-            while($image=readdir($d))
+            while($image = readdir($d))
             {
-                if ($image[0] != "." && substr_compare($image, "mini_", 0,4))
-                {
-                    if ($compte == 0){
-                        $tableau .= "<ul class='features'>";
-                    }
-                    if (!file_exists($dossier_produits."/mini_".$image)){
-                        creationVignette($image,300,180);
-                    }
+                if($image[0] != "." && substr_compare($image, "mini_", 0, 4)){
 
-                    if (file_exists($dossier_produits."/mini_".$image)){
-                        $vignette = '<img width="300" height="180" src="'.$dossier_produits."/mini_".$image.'"><h3>'.$image.'</h3>';
-                    }else{
-                        $vignette = '<svg xmlns="http://www.w3.org/2000/svg" width="300" height="180" viewBox="0 0 300 180">
-                        <rect fill="#ddd" width="300" height="180"/>
-                        <text fill="rgba(0,0,0,0.5)" font-family="sans-serif" font-size="30" dy="10.5" font-weight="bold" x="50%" y="50%" text-anchor="middle">300×180</text>
-                      </svg>
-                      ';
-                    }
-                    
-                    $tableau .='<li>'.$vignette.'</li>';
-                    $compte++;
-                    if ($compte >= 3){
-                        $compte = 0;
-                        $tableau .= "</ul>";
+                    $extension = explode('.', $image);
+                    $e = strtolower($extension[1]);
+
+                    if (in_array($e, $extension_valide))
+                    {
+                        $vignette = "<img width='300' height='180' src='".$dossier_produits."/mini_".$image."'>";
+
+                        if(!file_exists($dossier_produits."/mini_".$image)){
+                            if (!creationVignette($dossier_produits, $image, $e, 300, 180))
+                            {
+                                $vignette = '<svg xmlns="http://www.w3.org/2000/svg" width="300" height="180" viewBox="0 0 300 180">
+                                <rec fill="#ddd" width="300" heigh="180"/>
+                                <text fill="rgba(0,0,0,0.5)" font-family="sans-serif" font-size="30" dy="10.53" font-weight="bold" x="50%" y="50%" text-anchor="middle">300x180</text>
+                                </svg>';
+                            }
+                        }
+                        $tableau .= "<li>$vignette<h3>$image</h3></li>";    
                     }
                 }
             }
         }
-
-//        $tableau = "<table>$tableau</table>";
-    }else{
-        $tableau = "<table><tr><td>pas de produits</tr></table>";
+    } else {
+        $tableau .= "<li>Pas de produit à afficher</li>";
     }
+
+    $tableau .= "</ul>";
+
     return [
         'template' => 'catalogue',
         'titre' => "Le catalogue de nos produits",
         'sous-titre' => "Tout est fait main",
         'titre-court' => "Catalogue",
         'tableau' => $tableau,
-        'contenu' => "Lorem ipsum toujours même pour le catalogue ...",
+        'contenu' => "Merci de votre visite.",
         'pied-de-page' => "Voici la fin de la page"
     ];
-}
+}   
