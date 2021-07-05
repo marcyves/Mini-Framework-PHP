aeffdbae6d736c0492c71f4c2650614e09000bbf
Marc Augier <m.augier@me.com>
2021-07-05 11:56:39 +0200

    Début du blog

---
+++ site-dynamique/controleurs/blog.php
@@ -0,0 +1,12 @@
+<?php
+
+function controleur()
+{
+    return [
+        'titre' => "Le Blog",
+        'sous-titre' => "Les derniers articles",
+        'titre-court' => "",
+        'contenu' => "Une longue liste de posts bientôt...",
+        'pied-de-page' => "Bientôt les archives ici"
+    ];
+}
